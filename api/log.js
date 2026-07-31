f9578f744c38279e54781745b74fd196586e057a
youjia031 <youjia031@gmail.com>
2026-07-31 14:51:44 +0800

    Update and rename data.js to log.js

--- api/log.js
+++ api/log.js
@@ -1,7 +1,6 @@
 // api/log.js
 export default function handler(req, res) {
     console.log('📝 收到 Log 請求');
-    console.log('🌐 來源:', req.headers.origin || 'unknown');
     console.log('📦 內容:', req.body);
     console.log('⏰ 時間:', new Date().toISOString());
 
